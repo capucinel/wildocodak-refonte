d8c4bd5c427423af5a4ebca97ac9b6639ec88b3b
Capucine Leclerc <capucine_49@hotmail.com>
2019-02-28 15:16:11 +0100

    css bio page

--- client/js/composants/bio.js
+++ client/js/composants/bio.js
@@ -22,7 +22,7 @@ export const bio = `
               <p>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or double click
                 me to add your own content and make changes to the font. Feel free to drag and drop me anywhere you like
                 on your page. I’m a great place for you to tell a story and let your users know a little more about you</p>
-              <p>
+              <p class="contact-btn">
                 <a href="form.html">
                   <button type="submit" class="btn btn-primary button ">Contact me</button>
                 </a>
